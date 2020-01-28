c49fe59eadd36c17b0a8f627ffbd03eeeb137ba4
Kadir Egilmez <kadir.egilmez@capgemini.com>
2020-01-28 20:57:49 +0000

    Completed module-1

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, { useState, Fragment } from "react";
+import quizData from './quiz_data.json'
+
+
+
+function Quiz () {
+const [quiz_question, setQuiz_question] = useState(1);
+    const { quiz_questions } = quizData;
+    return (
+        <div>
+            <p>{quiz_questions[0].instruction_text}</p>
+        </div>
+    )
+
+}
+
+export default Quiz;
