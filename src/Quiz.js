af2098e77a89809114c49b5c756156c1340d5c6e
Kadir Egilmez <kadir.egilmez@capgemini.com>
2020-01-28 21:49:53 +0000

    update

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,14 +11,18 @@ class Quiz extends Component {
         };
     };
 
-
   render() {
     const { quiz_questions } = quizData;
     const isQuizEnd = this.state.quiz_position-1 === quiz_questions.length
     return (
       <div className="QuizQuestion">
-        {isQuizEnd && <QuizEnd />}
-        <QuizQuestion quiz_question={quiz_questions[this.state.quiz_position-1]} />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={quiz_questions[this.state.quiz_position - 1]}
+          />
+        )}
       </div>
     );
   }
