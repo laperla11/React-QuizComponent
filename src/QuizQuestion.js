d6c85aaacc61dce4b0f0f2feb455b3235939d3f7
Kadir Egilmez <kadir.egilmez@capgemini.com>
2020-01-28 21:32:21 +0000

    complete module-4

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-let quizData = require("./quiz_data.json");
-
+import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
@@ -19,7 +18,7 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.quiz_question.answer_options[0]}</li>
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
           </ul>
         </section>
       </main>
---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+let quizData = require("./quiz_data.json");
+
+class QuizQuestionButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //   quiz_position: 1
+    };
+  }
+
+  render() {
+    return (
+      <li>
+        <button>{this.props.button_text}</button>
+      </li>
+    );
+  }
+}
+
+export default QuizQuestionButton;
